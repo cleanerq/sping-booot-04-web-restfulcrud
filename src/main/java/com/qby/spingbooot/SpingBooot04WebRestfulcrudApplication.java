74c6d74c3b6d8bb4215bf83d660d3d2fa12aadcf
cleanrq <qbyun130@163.com>
2020-08-04 16:58:16 +0800

    import

---
+++ src/main/java/com/qby/spingbooot/SpingBooot04WebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.qby.spingbooot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBooot04WebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBooot04WebRestfulcrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qby/spingbooot/SpingBooot04WebRestfulcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qby.spingbooot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBooot04WebRestfulcrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
