858b96ff7ee199bf6147a68995e46586e6062b8e
cleanrq <qbyun130@163.com>
2020-08-05 13:28:31 +0800

    import

--- src/main/java/com/qby/spingbooot/controller/HelloController.java
+++ src/main/java/com/qby/spingbooot/controller/HelloController.java
@@ -1,13 +1,32 @@
 package com.qby.spingbooot.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class HelloController {
 
+    @ResponseBody
     @RequestMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("/success")
+    public String success(Model model) {
+        model.addAttribute("hello", "hello");
+        List<String> list = new ArrayList();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        model.addAttribute("users", list);
+
+        return "success";
     }
 }
