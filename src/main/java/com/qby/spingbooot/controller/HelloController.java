7dafd010421342b879670858813fae4250397993
cleanrq <qbyun130@163.com>
2020-08-05 13:55:33 +0800

    import

--- src/main/java/com/qby/spingbooot/controller/HelloController.java
+++ src/main/java/com/qby/spingbooot/controller/HelloController.java
@@ -4,10 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 @Controller
 public class HelloController {
@@ -20,13 +18,8 @@ public class HelloController {
 
     @RequestMapping("/success")
     public String success(Model model) {
-        model.addAttribute("hello", "hello");
-        List<String> list = new ArrayList();
-        list.add("A");
-        list.add("B");
-        list.add("C");
-        model.addAttribute("users", list);
-
+        model.addAttribute("hello", "<h1>你好</h1>");
+        model.addAttribute("users", Arrays.asList("zhangsan", "lisi", "wangwu"));
         return "success";
     }
 }
