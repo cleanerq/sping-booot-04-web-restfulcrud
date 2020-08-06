e9385d309bf687ace4b39a540c02547d92178caa
cleanrq <qbyun130@163.com>
2020-08-06 17:40:16 +0800

    import

--- src/main/java/com/qby/spingbooot/controller/EmployeeController.java
+++ src/main/java/com/qby/spingbooot/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.qby.spingbooot.controller;
 
+import com.qby.spingbooot.dao.DepartmentDao;
 import com.qby.spingbooot.dao.EmployeeDao;
+import com.qby.spingbooot.entities.Department;
 import com.qby.spingbooot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,9 @@ public class EmployeeController {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
     // 查询所有员工返回列表页面
     @GetMapping("/emps")
     public String list(Model model) {
@@ -26,5 +31,15 @@ public class EmployeeController {
         // 前缀 classpath:/templates/
         // 后缀 .html
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model) {
+        // 来到添加页面
+        // 查询所有部门在页面显示用
+
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emp/add";
     }
 }
